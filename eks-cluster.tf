42d584c085425fe07df8256154b71ec64e4d311d
Thomas Runyon <runyontr@gmail.com>
2021-04-26 06:02:11 -0400

    initial eks module

---
+++ eks-cluster.tf
@@ -0,0 +1,45 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.19"
+  subnets         = module.vpc.private_subnets
+
+  tags = {
+    Environment = "Leapfrog-Dev"
+    GithubOrg   = "leapfrogai"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "m5.xlarge"
+      additional_userdata           = "sysctl -w vm.max_map_count=262144"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "m5.2xlarge"
+      additional_userdata           = "sysctl -w vm.max_map_count=262144"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+  ]
+
+  worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  map_users                            = var.map_users
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
